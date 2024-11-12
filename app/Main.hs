1e463ed4f91064196ccad5714a42f4168cc290ff
King-141319 <kuli141319@gmail.com>
2024-11-12 18:35:37 +0000

    move the origin of trees to the left corder

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,43 @@
   
 import Graphics.Gloss
 import Graphics.Gloss.Data.Vector
--- import Graphics.Gloss
--- import Graphics.Gloss.Data.Vector
-  
--- -- Main function to display the tree
--- main :: IO ()
--- main = display (InWindow "Pythagorean Tree" (800, 800) (10, 10)) white (pictures [color red $ circle 2, polygon[(0,0),(0,50),(50,50),(50,0)], drawTree 10 (0,50) (50,50) 53.13])
--- test1
--- Function to draw the tree fractal
+import System.Random()
+import System.IO.Unsafe()
+import Graphics.Gloss.Interface.Pure.Display()
+
+
 drawTree :: Int -> Point -> Point -> Float -> Picture
-drawTree 0 _ _ _ = Blank  -- Base case: no more recursion when depth is 0
-drawTree depth (xa , ya) (xb , yb) angle =
-    pictures[ 
-      color white $ polygon[
-            (xa , ya),
+drawTree 0 _ _ _ = Blank  -- 基本情况：深度为 0 时停止递归
+drawTree depth (xa, ya) (xb, yb) angle =
+    pictures [ 
+      color (treeColor depth) $ polygon [
+            (xa, ya),
             (xa + lddx, ya + lddy),
-            (xa + ldx + lddx , ya + ldy + lddy),
-            (xa + ldx , ya + ldy)
+            (xa + ldx + lddx, ya + ldy + lddy),
+            (xa + ldx, ya + ldy)
             ],
-      color white $ polygon[
-            (xb , yb),
-            (xb + rddx , yb + rddy),
-            (xb + rdx + rddx , yb + rdy + rddy),
-            (xb + rdx , yb + rdy)
+      color (treeColor depth) $ polygon [
+            (xb, yb),
+            (xb + rddx, yb + rddy),
+            (xb + rdx + rddx, yb + rdy + rddy),
+            (xb + rdx, yb + rdy)
             ],
-      drawTree (depth - 1) (xa + ldx, ya + ldy) (xa + ldx + lddx , ya + ldy + lddy) angle,
-      drawTree (depth - 1) (xb + rdx + rddx , yb + rdy + rddy) (xb + rdx, yb + rdy) angle]
--- 
-      where
-            (ldx , ldy) = rotateV (0.5 * pi) (lddx, lddy)
-            (lddx , lddy) = rotateV (angle / 180 * pi) ((cos(angle / 180 * pi))*(xb-xa),(cos(angle / 180 * pi))*(yb-ya))
-            (rdx , rdy) = rotateV (-0.5 * pi) (rddx, rddy)
-            (rddx , rddy) = rotateV (angle / 180 * pi - 0.5 * pi) ((sin(angle / 180 * pi)) * (xa - xb) , (sin(angle / 180 * pi)) * (ya - yb))
+      drawTree (depth - 1) (xa + ldx, ya + ldy) (xa + ldx + lddx, ya + ldy + lddy) angle,
+      drawTree (depth - 1) (xb + rdx + rddx, yb + rdy + rddy) (xb + rdx, yb + rdy) angle
+    ]
+  where
+    -- 计算左、右分支的偏移量
+    (ldx, ldy) = rotateV (0.5 * pi) (lddx, lddy)
+    (lddx, lddy) = rotateV (angle / 180 * pi) ((cos (angle / 180 * pi)) * (xb - xa), (cos (angle / 180 * pi)) * (yb - ya))
+    (rdx, rdy) = rotateV (-0.5 * pi) (rddx, rddy)
+    (rddx, rddy) = rotateV (angle / 180 * pi - 0.5 * pi) ((sin (angle / 180 * pi)) * (xa - xb), (sin (angle / 180 * pi)) * (ya - yb))
+
+-- 定义颜色渐变函数，根据深度控制颜色变化
+treeColor :: Int -> Color
+treeColor depth = makeColor (0.2 + 0.8 * depthRatio) (0.5 * depthRatio) (0.8 * (1 - depthRatio)) 1
+  where
+    maxDepth = 10  -- 你可以设置最大递归深度为 10 或其他值，以控制颜色变化的速度
+    depthRatio = fromIntegral depth / fromIntegral maxDepth
 
 
 -- 定义窗口大小和标题
@@ -87,18 +92,18 @@ autumnScene = pictures
 winterScene :: Picture
 winterScene = pictures
   [ color (light cyan) $ rectangleSolid 800 600
-  , color white $ translate (-150) (-150) $ circleSolid 60 -- 雪人身体
-  , color white $ translate (-150) (-80) $ circleSolid 40  -- 雪人头部
-  , color (white) $ translate 0 (-200) $ rectangleSolid 800 20 -- 积雪
+  , color white $ translate (200) (-100) $ circleSolid 60 -- 雪人身体
+  , color white $ translate (200) (-30) $ circleSolid 40  -- 雪人头部
+  , color (white) $ translate 0 (-170) $ rectangleSolid 800 20 -- 积雪
   ]
 
 -- 根据不同季节返回不同的场景
 seasonScene :: Int -> Picture
-seasonScene 0 = pictures [ springScene,color white $ polygon[(0,0),(0,50),(50,50),(50,0)], drawTree 2 (0,50) (50,50) 45]
-seasonScene 1 = pictures [summerScene,color white $ polygon[(0,0),(0,50),(50,50),(50,0)], drawTree 6 (0,50) (50,50) 53.13]
-seasonScene 2 = pictures [autumnScene,color white $ polygon[(0,0),(0,50),(50,50),(50,0)], drawTree 4 (0,50) (50,50) 53.13]
-seasonScene 3 = pictures [winterScene,color white $ polygon[(0,0),(0,50),(50,50),(50,0)], drawTree 1 (0,50) (50,50) 53.13]
-seasonScene _ = pictures [springScene,color white $ polygon[(0,0),(0,50),(50,50),(50,0)], drawTree 2 (0,50) (50,50) 45]
+seasonScene 0 = pictures [springScene,color brown $ polygon[(-200,-100),(-200,-60),(-160,-60),(-160,-100)], drawTree 2 (-200,-60) (-160,-60) 45]
+seasonScene 1 = pictures [summerScene,color brown $ polygon[(-200,-100),(-200,-50),(-150,-50),(-150,-100)], drawTree 7 (-200,-50) (-150,-50) 55]
+seasonScene 2 = pictures [autumnScene,color brown $ polygon[(-200,-100),(-200,-40),(-140,-40),(-140,-100)], drawTree 4 (-200,-40) (-140,-40) 53.13]
+seasonScene 3 = pictures [winterScene,color brown $ polygon[(-200,-100),(-200,-40),(-140,-40),(-140,-100)], drawTree 1 (-200,-40) (-140,-40) 53.13]
+seasonScene _ = pictures [springScene,color brown $ polygon[(-200,-100),(-200,-40),(-140,-40),(-140,-100)], drawTree 2 (-200,-40) (-140,-40) 45]
 
 -- 绘制窗外四季景色
 drawScene :: Int -> Picture
@@ -110,7 +115,10 @@ drawScene season = pictures
 
 -- 主函数，轮流展示四季景色
 main :: IO ()
-main = simulate window background 20 0 render update
+main = simulate window background 1 (0, 0) render update
   where
-    render season = drawScene (season `mod` 4)
-    update _ _ season = (season + 1) `mod` 4
+    render (season, _) = drawScene (season `mod` 4)
+    update _ _ (season, counter) = 
+      if counter >= 1  -- Adjust this value to slow down further
+      then ((season + 1) `mod` 4, 0)  -- Reset counter and change season
+      else (season, counter + 1)  -- Increment counter only
